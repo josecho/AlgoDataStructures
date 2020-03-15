34afa6282676b6c73780e9a6ae830be7703e8c1e
Jose Villaverde <jlvbalsa@gmail.com>
2020-03-15 19:52:52 +0100

    Anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,45 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+//https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/RegExp
+// const word = "HI THERE!!!!!"
+// word.replace(/[^\w]/g, "").toLowerCase();
+
+function anagrams(stringA, stringB) {
+  return cleanString(stringA) === cleanString(stringB);
+}
+
+function cleanString(str) {
+  return str
+    .replace(/[^\w]/g, "")
+    .toLowerCase()
+    .split("")
+    .sort()
+    .join("");
+}
 
 module.exports = anagrams;
+
+// function anagrams(stringA, stringB) {
+//   const aCharMap = buildcharMap(stringA);
+//   const bCharMap = buildcharMap(stringB);
+//
+//   if (Object.keys(aCharMap).length != Object.keys(bCharMap).length) {
+//     return false;
+//   }
+//
+//   for (let char in aCharMap) {
+//     if (aCharMap[char] !== bCharMap[char]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+//
+// function buildcharMap(str) {
+//   const charMap = {};
+//   for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
+//     charMap[char] = charMap[char] + 1 || 1;
+//   }
+//   return charMap;
+// }
