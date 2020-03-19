a6c1c699900be1e8b5e7965e98edcc8539c0b1c5
Jose Villaverde <jlvbalsa@gmail.com>
2020-03-19 12:34:42 +0100

    Find The Vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,29 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  const matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0;
+}
 
 module.exports = vowels;
+
+// function vowels(str) {
+//   let count = 0;
+//   for (let char of str.toLowerCase()) {
+//     count += ["a", "e", "i", "o", "u"].includes(char) ? 1 : 0;
+//   }
+//   return count;
+// }
+
+// function vowels(str) {
+//   let count = 0;
+//   const checker = ["a", "e", "i", "o", "u"];
+//
+//   for (let char of str.toLowerCase()) {
+//     if (checker.includes(char)) {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
