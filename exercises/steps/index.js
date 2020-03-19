c908e551c36e4cd333c222b9d652cc53c963be98
Jose Villaverde <jlvbalsa@gmail.com>
2020-03-19 09:22:43 +0100

    Printing Steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,39 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n, row = 0, stair = "") {
+  if (n === row) {
+    return;
+  }
+
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+
+  // if (stair.length <= row) {
+  //   stair += "#";
+  // } else {
+  //   stair += " ";
+  // }
+  // steps(n, row, stair);
+
+  const add = stair.length <= row ? "#" : " ";
+  steps(n, row, stair + add);
+}
 
 module.exports = steps;
+
+// function steps(n) {
+//   for (row = 0; row < n; row++) {
+//     let stair = "";
+//     for (col = 0; col < n; col++) {
+//       if (col <= row) {
+//         stair += "#";
+//       } else {
+//         stair += " ";
+//       }
+//     }
+//     console.log(stair);
+//   }
+// }
