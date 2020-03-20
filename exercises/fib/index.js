ffef75766aaeec6ce2d0fd5d16b01b9d560e411d
Jose Villaverde <jlvbalsa@gmail.com>
2020-03-20 21:07:56 +0100

    Runtime Complexity in Practice - Fibonacci: Linear Runtime complexity

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,28 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function fib(n) {
+  const result = [0, 1];
+
+  for (let i = 2; i <= n; i++) {
+    const a = result[i - 1];
+    const b = result[i - 2];
+    result.push(a + b);
+  }
+  return result[n];
+}
 
 module.exports = fib;
+
+// For every increase in n we have to calculate one additional number.
+// This is a linear runtime algorith
+// function fib(n) {
+//   const result = [0, 1];
+//
+//   for (let i = 2; i <= n; i++) {
+//     const a = result[i - 1];
+//     const b = result[i - 2];
+//     result.push(a + b);
+//   }
+//   return result[n];
+// }
